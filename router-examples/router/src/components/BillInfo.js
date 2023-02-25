37661a67bd6905834718a5913efdbee27adb5fd1
Ben Gordon <ben.gordon@utexas.edu>
2023-02-24 21:16:46 -0600

    good stopping place

--- router-examples/router/src/components/BillInfo.js
+++ router-examples/router/src/components/BillInfo.js
@@ -6,6 +6,24 @@ export default function BillInfo(props) {
         <h2>{props.bill.title}</h2> :
         <h3>{props.bill.title}</h3>
       }
+      <p>House Vote</p>
+      {props.bill.houseVote && <div className="voteTotal flexRow">
+        <div style={{width: props.bill.houseVote[0], backgroundColor: "#8f8"}}>
+          {props.bill.houseVote[0]}
+        </div>
+        <div style={{width: props.bill.houseVote[1], backgroundColor: "#f88"}}>
+          {props.bill.houseVote[1]}
+        </div>
+      </div>}
+      <p>Senate Vote</p>
+      {props.bill.senateVote && <div className="voteTotal flexRow">
+        <div style={{width: props.bill.senateVote[0]*4.35, backgroundColor: "#8f8"}}>
+          {props.bill.senateVote[0]}
+        </div>
+        <div style={{width: props.bill.senateVote[1]*4.35, backgroundColor: "#f88"}}>
+          {props.bill.senateVote[1]}
+        </div>
+      </div>}
       {props.navigate && <button onClick={() => props.navigate(path)}>More info</button>}
       {props.sign && props.veto && <div className="flexRow billActions">
         <button onClick={props.sign} style={{backgroundColor: "#9f9"}}>Sign</button>
