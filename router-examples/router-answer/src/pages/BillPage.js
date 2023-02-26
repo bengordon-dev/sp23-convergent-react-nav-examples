0f47c27595e733b52624448e054ca827676e4471
Ben Gordon <ben.gordon@utexas.edu>
2023-02-26 12:50:51 -0600

    one last touch

--- router-examples/router-answer/src/pages/BillPage.js
+++ router-examples/router-answer/src/pages/BillPage.js
@@ -6,6 +6,10 @@ export default function BillPage(props) {
     <div className="page flexCenter">
       <h1>{props.data.title}</h1>
       <button className="nav" onClick={() => navigate("/bills")}>Go Back</button>
+      <p>Sponsors:</p>
+      {props.data.sponsors && <ul>
+        {props.data.sponsors.map((e, i) => <li key={i}>{e}</li>)}
+      </ul>}
     </div>
   )
 }
--- router-examples/router/src/pages/BillPage.js
+++ router-examples/router/src/pages/BillPage.js
@@ -6,6 +6,10 @@ export default function BillPage(props) {
     <div className="page flexCenter">
       <h1>{props.data.title}</h1>
       <button className="nav" onClick={() => alert("Implement me")}>Go Back</button>
+      <p>Sponsors:</p>
+      {props.data.sponsors && <ul>
+        {props.data.sponsors.map((e, i) => <li key={i}>{e}</li>)}
+      </ul>}
     </div>
   )
 }
